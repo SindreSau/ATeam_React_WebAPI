7aee521825418b03119bb1558212960854729e06
SindreSau <sindre_50@hotmail.com>
2024-11-10 09:26:11 +0100

    feat: example of how we could render two very different behaviours in the same "page" ("/products")

--- react-frontend/src/pages/Products.tsx
+++ react-frontend/src/pages/Products.tsx
@@ -1,11 +1,31 @@
+import {useAuthContext} from "../contexts/AuthContext";
+
 const Products = () => {
+    const { user, isLoading } = useAuthContext();
+
     return (
         <div className="">
             <h1 className="">Products</h1>
 
-            <p className="">
-                This is the products page.
-            </p>
+            {isLoading ? (
+                <p>Loading...</p>
+            ) : (
+                // load admin or vendor products based on user.role
+                <>
+                    {user?.role === 'Admin' && (
+                        <div className="">
+                            <p className="">You are signed in as an administrator.</p>
+                            <span>Render ADMIN component here</span>
+                        </div>
+                    )}
+                    {user?.role === 'Vendor' && (
+                        <div className="">
+                            <p className="">Welcome back, vendor!</p>
+                            <span>Render VENDOR component here</span>
+                        </div>
+                    )}
+                </>
+            )}
         </div>
     );
 };
